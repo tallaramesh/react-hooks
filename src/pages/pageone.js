fd5336cb62a8219c57b8f911ee6c914aa126e29a
TR <tallaramesh040@gmail.com>
2022-11-03 16:35:25 +0530

    new line9 added

--- src/pages/pageone.js
+++ src/pages/pageone.js
@@ -39,6 +39,9 @@ const PageOne = () => {
                 <div className="item item5">item5</div>
                 <div className="item item6">item6</div>
                 <div className="item item7">item7</div>
+
+                <div className="item item7">item9</div>
+                
             </div>
         </>
     )
